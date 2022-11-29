2d69bc8e875fef83235c963a7006e2181150bd16
Zyblyuk <qikroman@gmail.com>
2022-11-29 18:18:43 +0300

    Testing different regression models

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
 from sklearn.ensemble import ExtraTreesRegressor
@@ -9,6 +10,7 @@ from sklearn.metrics import mean_squared_error
 from datetime import date
 from datetime import datetime
 
+import seaborn as sn
 import matplotlib.pyplot as plt
 
 
@@ -25,32 +27,51 @@ def transform_date(dataframe):
     return dataframe
 
 
-dataset = pd.read_csv('income-prediction/train.csv')
+if __name__ == '__main__':
+    dataset = pd.read_csv('income-prediction/train.csv')
 
-dataset['City Group'] = (dataset['City Group'] == 'Big Cities').astype(int)
-dataset['Type'] = (dataset['Type'] == 'IL').astype(int)
+    dataset['City Group'] = (dataset['City Group'] == 'Big Cities').astype(int)
+    dataset['Type'] = (dataset['Type'] == 'IL').astype(int)
 
-dataset.drop(['City', 'Id'], axis=1, inplace=True)
+    dataset.drop(['City', 'Id'], axis=1, inplace=True)
 
-dataset = transform_date(dataset)
-columns_len = dataset.columns.shape[0]
+    dataset = transform_date(dataset)
 
-X = dataset.iloc[:, 0:columns_len - 1]
-Y = dataset.iloc[:, columns_len - 1]
+    corr_matrix = dataset.corr()
+    plt.figure(figsize=(20, 20))
+    sn.heatmap(corr_matrix, annot=True)
+    plt.show()
 
+    drop = []
+    for i in corr_matrix.columns.drop('revenue'):
+        corr = corr_matrix[i].index
+        for a in corr:
+            if corr_matrix[i][a] > 0.9 and i != a:
+                if corr_matrix['revenue'][i] > corr_matrix['revenue'][a]:
+                    drop.append(a) if a not in drop else 0
+                else:
+                    drop.append(i) if i not in drop else 0
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=8)
+    dataset.drop(drop, axis=1, inplace=True)
 
-cls = ExtraTreesRegressor()
-cls.fit(X_train, y_train)
+    largest = corr_matrix['revenue'][np.abs(corr_matrix['revenue'] > 0.05)].index
+    largest = [i for i in largest if i not in drop]
+    dataset = dataset[largest]
+    dataset.head()
 
-print(cls.feature_importances_)
-feat_importances = pd.Series(cls.feature_importances_, index=X.columns)
-feat_importances.nlargest(8).plot(kind='barh')
-plt.show()
+    scale = MinMaxScaler()
 
-y_predict_test = cls.predict(X_test)
+    for col in dataset.columns.drop(['revenue']):
+        dataset[col] = scale.fit_transform(dataset[col].values.reshape(-1, 1))
 
-print(f'\nRandomForestRegressor Test date:\n'
-      f'RMSE:\t{np.sqrt(mean_squared_error(y_test, y_predict_test))}')
+    columns_len = dataset.columns.shape[0]
 
+    X = dataset.iloc[:, 0:columns_len - 1]
+    Y = dataset.iloc[:, columns_len - 1]
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=8)
+
+    cls = ExtraTreesRegressor()
+    cls.fit(X_train, y_train)
+
+    y_predict_test = cls.predict(X_test)
+    print(f'RMSE:\t{np.sqrt(mean_squared_error(y_test, y_predict_test))}')
